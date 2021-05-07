06ff3f704ae280cf0dfdd70ada0ce4d6030a9f14
catvirt <82500684+catvirt@users.noreply.github.com>
2021-05-08 00:39:31 +0300

    Add files via upload

---
+++ Code/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Leaf.xNet;
+
+namespace RepoDumperionio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(@"
+
+ /$$$$$$$                                                    /$$                                    
+| $$__  $$                                                  | $$                                    
+| $$  \ $$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$ | $$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$ 
+| $$$$$$$/ /$$__  $$ /$$__  $$ /$$__  $$ /$$_____/ /$$__  $$| $$  /$$/ /$$_____/ /$$__  $$ /$$__  $$
+| $$__  $$| $$$$$$$$| $$  \ $$| $$  \ $$|  $$$$$$ | $$$$$$$$| $$$$$$/ |  $$$$$$ | $$$$$$$$| $$  \__/
+| $$  \ $$| $$_____/| $$  | $$| $$  | $$ \____  $$| $$_____/| $$_  $$  \____  $$| $$_____/| $$      
+| $$  | $$|  $$$$$$$| $$$$$$$/|  $$$$$$/ /$$$$$$$/|  $$$$$$$| $$ \  $$ /$$$$$$$/|  $$$$$$$| $$      
+|__/  |__/ \_______/| $$____/  \______/ |_______/  \_______/|__/  \__/|_______/  \_______/|__/      
+                    | $$                                                                            
+                    | $$                                                                            
+                    |__/                                                                            
+
+            ");
+
+
+            Console.WriteLine(" Enter REPO URL: ");
+
+            string repo = Console.ReadLine();
+
+            Console.WriteLine(" Enter API KEY: ");
+
+            string apikey = Console.ReadLine();
+
+            try
+            {
+                HttpRequest req = new HttpRequest
+                {
+                    AcceptEncoding = "gzip, deflate",
+                    Authorization = apikey
+                };
+
+                var GetFilesZip = req.Get(repo, null).ToBytes();
+
+                var zip = new ZipArchive(new MemoryStream(GetFilesZip), ZipArchiveMode.Read);
+
+                foreach (var entry in zip.Entries)
+                {
+                    Directory.CreateDirectory(entry
+                        .ToString()
+                        .Substring(0, entry
+                        .ToString()
+                        .IndexOf("\\")));
+
+                    File.AppendAllText($"{entry}", new StreamReader(entry
+                        .Open())
+                        .ReadToEnd());
+                }
+            }
+            catch  { throw new Exception(" Error while dumping!");  }
+
+            Console.WriteLine("Configs DUMPED! Press ANY key to close!");
+
+            Console.Read();
+
+        }
+    }
+}
