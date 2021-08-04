f5b132799125b6bbdf01907ba5610556464cc49c
catvirt <82500684+catvirt@users.noreply.github.com>
2021-08-04 22:35:03 +0300

    Add files via upload

---
+++ DumpBullet/DumpBullet/Program.cs
@@ -0,0 +1,49 @@
+﻿using Leaf.xNet;
+using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace DumpBullet
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Shit code but it works!
+        /// </summary>
+        private static void Main()
+        {
+
+            Console.WriteLine(" Input the repostory url: ");
+
+            var readUrl = Console.ReadLine();
+
+            Console.WriteLine(" Input the repostory API key: ");
+
+            var apiKey = Console.ReadLine();
+
+            try
+            {
+
+                var _client = new HttpRequest() { Authorization = apiKey };
+
+                var getConfigs = _client.Get(readUrl).ToBytes();
+
+                var zip = new ZipArchive(new MemoryStream(getConfigs), ZipArchiveMode.Read);
+
+                Directory.CreateDirectory(zip.Entries.ToString().Substring(0, zip.Entries.ToString().IndexOf("\\")));
+
+                foreach (var entry in zip.Entries)
+                {
+                    File.AppendAllText($"{entry}", new StreamReader( stream: entry.Open() ).ReadToEnd());
+                }
+
+            }
+            catch
+            {
+                throw new Exception(" Error meanwhile I tried to dump the repo!");
+            }
+
+            Console.WriteLine(" Repo dump was a success! ");
+        }
+    }
+}
